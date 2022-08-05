8f2c0977f76faa89a5ad5e164928ddf377c7ca95
simeunseo <rkdcjfehowl@naver.com>
2022-08-05 16:10:09 +0900

    Remove: migration 파일 삭제

--- posts/admin.py
+++ posts/admin.py
@@ -1,7 +1,4 @@
 from django.contrib import admin
-
-#posts.admin.py 
-
 from .models import Post
 
 @admin.register(Post)
--- users/models.py
+++ users/models.py
@@ -1,7 +1,8 @@
-# users/models.py
+import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     desc = models.CharField(max_length=25, null=True, blank=True)
     #birth_date = models.DateField(verbose_name='생년월일')
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
