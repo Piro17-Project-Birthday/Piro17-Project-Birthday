019b14ead4b1920e97ed03c545718d9a619d606c
simeunseo <rkdcjfehowl@naver.com>
2022-08-07 14:14:25 +0900

    Feat: 생일까지 남은 날짜에 따른 구분 기능

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from datetime import datetime
 from .models import BirthdayPage
 from .models import Post
 from .forms import PostForm
@@ -6,8 +7,8 @@ from .forms import PostForm
 def createBirthdayPage(request):
     if request.user.is_authenticated:
         if request.method == 'POST':
-            BirthdayPage.objects.create(owner=request.user)
-            return redirect("/")
+            birthday_page = BirthdayPage.objects.create(owner=request.user)
+            return redirect(f"/{birthday_page.id}")
     
         context={
             "request_user" : request.user
@@ -17,9 +18,34 @@ def createBirthdayPage(request):
         return redirect("/login")
     
 def detailBirthdayPage(request,pk):
-    birthdayPage = BirthdayPage.objects.get(pk=pk)
+    birthday_page = BirthdayPage.objects.get(pk=pk)
+    name = birthday_page.owner.full_name
+
+    birthday_month = birthday_page.owner.birthday.month #생일자의 생일 월
+    birthday_day = birthday_page.owner.birthday.day #생일자의 생일 일
+    today = datetime.now().date() #현재 날짜
+    today_year = today.year #현재 년도
+
+    birthday_thisyear = datetime.strptime(str(today_year)+str(birthday_month)+str(birthday_day), "%Y%m%d").date() #올해 생일
+    birthday = birthday_thisyear #생일은 올해 생일로 초기화한다
+    date_diff = abs((today-birthday).days) 
+    if birthday_thisyear < today : #올해 생일이 이미 지났다면
+        birthday = datetime.strptime(str(today_year+1)+str(birthday_month)+str(birthday_day), "%Y%m%d").date() #생일을 내년 생일로 한다
+        birthday_state = "passed"
+        date_diff = abs((today-birthday).days) 
+    else : #올해 생일이 아직 오지 않았다면
+        if date_diff == 0 : #생일이 오늘이라면
+            birthday_state = "today"
+        elif date_diff <= 7: #생일이 7일 이내로 남았다면
+            birthday_state = "upcoming"
+        else : #생일이 7일 넘게 남았다면
+            birthday_state = "waiting"
+        
     context = {
-        "birthdayPage" : birthdayPage
+        "name" : name,
+        "birthday" : birthday,
+        "date_diff" : date_diff,
+        "birthday_state" : birthday_state
     }
     return render(request, template_name="posts/detail_birthday_page.html", context=context)
     
--- users/models.py
+++ users/models.py
@@ -3,9 +3,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    email = models.EmailField(null=True)
-    full_name = models.CharField(null=True,max_length=50)
-    birthday = models.DateField(null=True)
+    email = models.EmailField(null=True, blank=True)
+    full_name = models.CharField(null=True, blank=True, max_length=50)
+    birthday = models.DateField(null=True, blank=True)
 
     
 	
