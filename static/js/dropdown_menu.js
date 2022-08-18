3c0fea9bc258d5ade5e00e3a61bf75831eb77c1a
simeunseo <rkdcjfehowl@naver.com>
2022-08-19 02:37:05 +0900

    Delete: dropdown.js 삭제

--- static/js/dropdown_menu.js
+++
@@ -1,8 +0,0 @@
-function dropdown_menu() {
-  let click = document.getElementById("dropdown_content");
-  if (click.style.display === "none") {
-    click.style.display = "block";
-  } else {
-    click.style.display = "none";
-  }
-}
