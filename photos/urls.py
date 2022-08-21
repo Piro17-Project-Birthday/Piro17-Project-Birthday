1a5c6683c8e0e313a8c426ef09474f03065e1269
DongHeonLee <eastlaw80@naver.com>
2022-08-22 03:32:53 +0900

    Fix. re_path를 url로 이용

--- photos/urls.py
+++ photos/urls.py
@@ -3,6 +3,7 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
+from django.urls import re_path as url
 
 app_name = "photos"
 
@@ -10,4 +11,5 @@ urlpatterns = [
     path("<int:year>/<uuid:pk>/photo/", views.photoHome, name="photo_home"),
     path("<int:year>/<uuid:pk>/photo/write", views.photoCreate, name="photo_create"),
     path("<int:pk>/photo/delete/", views.photoDelete, name="photo_delete"),
-] + url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
