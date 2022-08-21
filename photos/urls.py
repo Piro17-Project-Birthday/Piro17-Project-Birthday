14b09ebae4ff39c497cfb59cce8a14db4ee16db4
DongHeonLee <eastlaw80@naver.com>
2022-08-22 02:59:34 +0900

    Fix. url import 없는 버전

--- photos/urls.py
+++ photos/urls.py
@@ -3,7 +3,6 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
-from django.conf.urls import url
 
 app_name = "photos"
 
