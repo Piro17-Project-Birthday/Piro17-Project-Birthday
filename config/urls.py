6643dd012a2f8491f8c7ed6b0f941f42855d929c
DongHeonLee <eastlaw80@naver.com>
2022-08-22 00:42:43 +0900

    Fix : url에 Media관련 사항 추가

--- config/urls.py
+++ config/urls.py
@@ -26,4 +26,4 @@ urlpatterns = [
     path('account/', include('allauth.urls')),
     path('', include('photos.urls')),
     path('', include('tmies.urls')),
-] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root = settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
