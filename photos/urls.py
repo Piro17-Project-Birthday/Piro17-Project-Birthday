672f4e0e54d12f2b9e6c5e974abaafcf7732c7b4
DongHeonLee <eastlaw80@naver.com>
2022-08-22 02:41:40 +0900

    Fix. url로 원위치

--- photos/urls.py
+++ photos/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path, re_path
+from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.static import serve
+from django.conf.urls import url
 
 app_name = "photos"
 
@@ -10,4 +11,4 @@ urlpatterns = [
     path("<int:year>/<uuid:pk>/photo/", views.photoHome, name="photo_home"),
     path("<int:year>/<uuid:pk>/photo/write", views.photoCreate, name="photo_create"),
     path("<int:pk>/photo/delete/", views.photoDelete, name="photo_delete"),
-] + re_path(r'^media/(?P<path>.\*)$', serve, {'document_root': settings.MEDIA_ROOT,})
+] + url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})
